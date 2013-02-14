c19a7d6665a00bb6331c9fa02fe2691828df3ab8
Rodrigo Moya <rodrigo@gnome-db.org>
2013-02-14 16:39:22 +0100

    Revert "Add missing license file"
    This reverts commit 607faa3567d190158d6a7db1add6a14a186d2cd8.

--- npapi-plugin/src/object.c
+++ npapi-plugin/src/object.c
@@ -24,7 +24,6 @@
 
 #include "config.h"
 
-#include <string.h>
 #include "object.h"
 #include <glib.h>
 #include <gio/gio.h>
@@ -33,7 +32,6 @@
 typedef struct {
   NPObject object;
   GHashTable *methods;
-  GFileMonitor *monitor;
 } WebappObjectWrapper;
 
 typedef NPVariant (*WebappMethod) (NPObject *object,
@@ -436,53 +434,6 @@ uninstall_chrome_app_wrapper (NPObject *object,
   return result;
 }
 
-static void
-on_applications_dir_changed (GFileMonitor *monitor,
-			     GFile *file,
-			     GFile *other_file,
-			     GFileMonitorEvent event_type,
-			     gpointer user_data)
-{
-  if (event_type == G_FILE_MONITOR_EVENT_CREATED) {
-    gchar *contents;
-    gsize len;
-    GError *error = NULL;
-
-    g_debug ("File %s added\n", g_file_get_path (file));
-
-    if (g_file_get_contents (g_file_get_path (file), &contents, &len, &error)) {
-      gchar *tmp = contents;
-      const gchar *shebang = "#!/usr/bin/env xdg-open";
-
-      g_debug ("Old contents = %s\n", contents);
-
-      /* Read 1st line */
-      if (!strncmp (contents, shebang, strlen (shebang))) {
-	tmp += strlen (shebang);
-	if (*tmp == '[') {
-	  GString *new_contents = g_string_new (shebang);
-
-	  new_contents = g_string_append (new_contents, "\n");
-	  new_contents = g_string_append (new_contents, tmp);
-
-	  if (!g_file_set_contents (g_file_get_path (file), new_contents->str, new_contents->len, &error)) {
-	    g_warning ("Could not write %s file: %s", g_file_get_path (file), error->message);
-	    g_error_free (error);
-	  } else
-	    g_debug ("New contents: %s\n", new_contents->str);
-
-	  g_string_free (new_contents, TRUE);
-	}
-      }
-
-      g_free (contents);
-    } else {
-      g_warning ("Could not read %s file: %s", g_file_get_path (file), error->message);
-      g_error_free (error);
-    }
-  }
-}
-
 NPObject *
 webapp_create_plugin_object (NPP instance)
 {
@@ -493,32 +444,12 @@ webapp_create_plugin_object (NPP instance)
 
   WebappObjectWrapper *wrapper = (WebappObjectWrapper *) object;
 
-  /* Public methods */
   g_hash_table_insert (wrapper->methods,
 		       (gchar *) "installChromeApp",
 		       install_chrome_app_wrapper);
   g_hash_table_insert (wrapper->methods,
 		       (gchar *) "uninstallChromeApp",
 		       uninstall_chrome_app_wrapper);
-
-  /* Monitor applications directory */
-  GFile *file;
-  GError *error = NULL;
-  gchar *path;
-
-  path = g_strdup_printf ("%s/.local/share/applications", g_get_home_dir ());
-  file = g_file_new_for_path (path);
-
-  wrapper->monitor = g_file_monitor_directory (file, 0, NULL, &error);
-  if (wrapper->monitor) {
-    g_signal_connect (wrapper->monitor, "changed", G_CALLBACK (on_applications_dir_changed), wrapper);
-  } else {
-    g_error ("Error monitoring %s directory: %s\n", path, error->message);
-    g_error_free (error);
-  }
-
-  g_free (path);
-  g_object_unref (file);
 
   return object;
 }
